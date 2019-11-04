122c251d33076984df8848eb21595a96a365e5be
Brian Nicholls <nichol88@gmail.com>
2019-11-04 21:50:45 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -56,7 +56,7 @@ def consolidate_cart(cart)
   result
 end
 
-def apply_coupons(cart, coupons)
+def apply_coupons(cart = [], coupons = [])
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
