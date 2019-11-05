2d4a6c76c51efa5a3ea67beadce646e3ccc993dd
Brian Nicholls <nichol88@gmail.com>
2019-11-05 00:21:22 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -126,8 +126,8 @@ def apply_clearance(cart)
   count = 0
   while count < cart.length
     if cart[count][:clearance]
-      cart[count][:price] *= 0.8
-      cart[count][:price].ceil(2)
+      (cart[count][:price]*100) *= 0.8
+      cart[count][:price] = cart[count][:price].ceil(2) / 100
     end
     count += 1
   end
